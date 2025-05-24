cdeffa7afbd107990654a9341227d1da9e2d5acd
Harini050805 <harinigopi05@gmail.com>
2025-05-24 15:24:55 +0530

    day23_soln

--- submissions/Harini S/DAY 23/day23_word_collector.py
+++ submissions/Harini S/DAY 23/day23_word_collector.py
@@ -1,8 +1,6 @@
 import re
 from collections import Counter
 inp_str=input("Enter text:").lower()
-#l=list(map(str,inp_str.split()))
-#print(l)
 remove_punc=re.sub(r'[^\w\s]',' ',inp_str)
 count=Counter(remove_punc.split())
 for word,occurence in count.items():
