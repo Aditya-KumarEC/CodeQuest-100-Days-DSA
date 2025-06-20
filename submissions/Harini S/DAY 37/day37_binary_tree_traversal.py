baf990c60ed095496abf7ad98e6d6fddb1669fd5
Harini050805 <harinigopi05@gmail.com>
2025-06-20 23:13:05 +0530

    Update_day_37

--- submissions/Harini S/DAY 37/day37_binary_tree_traversal.py
+++ submissions/Harini S/DAY 37/day37_binary_tree_traversal.py
@@ -6,17 +6,17 @@ class node:
 def binarytree(values):
     if not values:
         return None
-    Nodes=[node(val) for val in values]
-    root=Nodes[0]
+    New_Nodes=[node(val) for val in values]
+    root=New_Nodes[0]
     i=0 #parent index
     j=1 #child index
     while j<len(values):
-        if i<len(Nodes):
-            if j<len(Nodes):
-                Nodes[i].left=Nodes[j]
+        if i<len(New_Nodes):
+            if j<len(New_Nodes):
+                New_Nodes[i].left=New_Nodes[j]
                 j+=1
-            if j<len(Nodes):
-                Nodes[i].right=Nodes[j]
+            if j<len(New_Nodes):
+                New_Nodes[i].right=New_Nodes[j]
                 j+=1
         i+=1
     return root
